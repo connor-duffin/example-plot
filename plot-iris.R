63e1df24a957503b9e36fb15116ba3fb212576df
Connor Duffin <connor.duffin@nhm.ac.uk>
2024-08-02 14:12:22 +0100

    First commit --- adding files.

---
+++ plot-iris.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+
+# Load data
+data(iris)
+head(iris)
+
+# Create a scatter plot using the iris dataset
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point() +
+  labs(title = "Sepal Length vs Sepal Width in Iris Dataset",
+       x = "Sepal Length (cm)",
+       y = "Sepal Width (cm)") +
+  theme_minimal()
